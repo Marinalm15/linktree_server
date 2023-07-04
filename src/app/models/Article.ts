46f96d318645bff3678fdf024ac35b5c1ad0ec65
Marina Mendonça <marinalm15@hotmail.com>
2023-07-04 16:37:04 -0300

    ajustes do mongoose

--- src/app/models/Article.ts
+++ src/app/models/Article.ts
@@ -5,6 +5,7 @@ export const Article = model(
   new Schema({
     name: { type: String, required: true },
     url: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
   })
 );
 
