913bb9f59500a0c6459c19813f8299adb05602bf
Marina Mendonça <marinalm15@hotmail.com>
2023-07-06 16:50:29 -0300

    validação de rotas privadas

--- src/app/useCases/articles/deleteArticle.ts
+++ src/app/useCases/articles/deleteArticle.ts
@@ -1,9 +1,16 @@
 import { Request, Response } from "express";
 import { Article } from "../../models/Article";
+import { isValidObjectId } from "mongoose";
 
 export async function deleteArticle(req: Request, res: Response) {
   try {
     const { articleId } = req.params;
+
+    const isValidId = isValidObjectId(articleId);
+
+    if (!isValidId) {
+      return res.status(400).json({ error: "O Id informado não é válido!" });
+    }
 
     await Article.findByIdAndDelete(articleId);
 
--- src/app/useCases/articles/updateArticle.ts
+++ src/app/useCases/articles/updateArticle.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
 import { Article } from "../../models/Article";
+import { isValidObjectId } from "mongoose";
 
 export async function updateArticle(req: Request, res: Response) {
   try {
     const { articleId } = req.params;
     const { name, url } = req.body;
+
+    const isValidId = isValidObjectId(articleId);
+
+    if (!isValidId) {
+      return res.status(400).json({ error: "O Id informado não é válido!" });
+    }
 
     await Article.findByIdAndUpdate(articleId, { name, url });
 
